bd75cd8e734363c65312c5d02ed7d6dd28ef60a1
Jordan McAdoo <jordan.mcadoo@stitchfix.com>
2018-11-02 17:28:01 -0500

    made an Xcode project if we decide to do native

---
+++ App/BorrowMyAngel/BorrowMyAngel/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  BorrowMyAngel
+//
+//  Created by Jordan McAdoo on 11/2/18.
+//  Copyright © 2018 hack4good. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ App/BorrowMyAngel/BorrowMyAngel/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  BorrowMyAngel
+//
+//  Created by Jordan McAdoo on 11/2/18.
+//  Copyright © 2018 hack4good. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
