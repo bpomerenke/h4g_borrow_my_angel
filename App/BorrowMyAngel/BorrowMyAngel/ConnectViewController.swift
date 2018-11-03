fc2e8800e63f026701c11bcb831823b63a4fd832
Ben Pomerenke <bpomerenke@gmail.com>
2018-11-03 13:15:20 -0500

    updated connect screen

--- App/BorrowMyAngel/BorrowMyAngel/ConnectViewController.swift
+++ App/BorrowMyAngel/BorrowMyAngel/ConnectViewController.swift
@@ -1,41 +1,18 @@
 import UIKit
 
 class ConnectViewController: UIViewController {
-    @IBOutlet weak var connectButton: UIButton!
-    @IBOutlet weak var connectDescription: UILabel!
-    @IBOutlet weak var issuePicker: UIPickerView!
     
-    var issueData: [String] = ["Depression", "I'm feeling sad", "I'm having a rough day", "Physical Abuse", "Mental Abuse", "Substance Abuse"]
-    
+    @IBOutlet weak var severitySlider: UISlider!
+    @IBOutlet weak var severityInput: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.issuePicker.delegate = self
-        self.issuePicker.dataSource = self
      
     }
     
     @IBAction func comeBack(unwindSegue: UIStoryboardSegue){
         print("did it...came back")
     }
-    
-    @IBAction func connectNow(_ sender: Any) {
-
-    }
-}
-
-extension ConnectViewController: UIPickerViewDelegate {
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return issueData[row]
-    }
-}
-
-extension ConnectViewController: UIPickerViewDataSource {
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return issueData.count
+    @IBAction func changeSeverity(_ sender: Any) {
+        severityInput.text = String(Int(severitySlider.value*10.0))
     }
 }
