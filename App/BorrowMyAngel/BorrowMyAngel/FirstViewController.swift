bbb7429fada45b918a5efccee6186ffcc92c110e
Ben Pomerenke <bpomerenke@gmail.com>
2018-11-02 22:58:17 -0500

    fake search

--- App/BorrowMyAngel/BorrowMyAngel/FirstViewController.swift
+++ App/BorrowMyAngel/BorrowMyAngel/FirstViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var progress: UIProgressView!
+    @IBOutlet weak var connectionStatus: UILabel!
+    @IBOutlet weak var connectButton: UIButton!
+    @IBOutlet weak var connectDescription: UILabel!
+    
+    var timer = Timer()
+    var progressVal = 00.0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.progress.progress = 00.75
     }
-
-
+    
+    @IBAction func connectNow(_ sender: Any) {
+        self.progress.isHidden = false
+        self.connectionStatus.isHidden = false
+        self.connectButton.isHidden = true
+        self.connectDescription.isHidden = true
+        timer = Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true, block: { (Timer) in
+            self.progressVal += 1
+            self.progress.progress = Float(self.progressVal / 100.0)
+            
+            if self.progressVal >= 100 {
+                Timer   .invalidate()
+                self.connectionStatus.text = "Found"
+            }
+        })
+    }
 }
+
 
