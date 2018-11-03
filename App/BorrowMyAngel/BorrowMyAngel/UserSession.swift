465701bfc013fb5c6a5cc1537aaabb22c50a6a69
Tom Pepe <tom.pepe@asynchrony.com>
2018-11-03 17:25:00 -0500

    Randomize person in needs name

--- App/BorrowMyAngel/BorrowMyAngel/UserSession.swift
+++ App/BorrowMyAngel/BorrowMyAngel/UserSession.swift
@@ -5,6 +5,7 @@
 //  Created by dev1 on 11/3/18.
 //  Copyright © 2018 hack4good. All rights reserved.
 //
+import SendBirdSDK
 
 class UserSession {
     static var sharedInstance = UserSession()
@@ -12,12 +13,29 @@ class UserSession {
         userSessionHandle = ""
     }
     var userSessionHandle: String
+    var sBDUser: SBDUser?
 
-    func setHandle(handle: String) {
-        userSessionHandle = handle
+    func setHandle(handle: String? = nil) {
+        guard handle == nil else {
+            userSessionHandle = handle!
+            return
+        }
+        userSessionHandle = ""
+    }
+
+    func setSBDUser(user: SBDUser?) {
+        sBDUser = user
     }
 
     func getHandle() -> String {
-        return userSessionHandle
+        if(isAngel()) {
+            return userSessionHandle
+        } else {
+            return "Person_In_Need" + NSUUID().uuidString
+        }
+    }
+
+    func isAngel() -> Bool {
+        return userSessionHandle.count > 0
     }
 }
