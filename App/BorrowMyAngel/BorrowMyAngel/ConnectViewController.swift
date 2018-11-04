b432535287855dcee71f1ee735f68fc81d9fef9a
Ben Pomerenke <bpomerenke@gmail.com>
2018-11-04 00:42:20 -0500

    finished view port

--- App/BorrowMyAngel/BorrowMyAngel/ConnectViewController.swift
+++ App/BorrowMyAngel/BorrowMyAngel/ConnectViewController.swift
@@ -3,20 +3,37 @@ import UIKit
 class ConnectViewController: UIViewController {
     
     @IBOutlet weak var severitySlider: UISlider!
+    @IBOutlet weak var severityLabel: UILabel!
     @IBOutlet weak var severityInput: UITextField!
     @IBOutlet weak var pickerInput: UIPickerView!
+    @IBOutlet weak var issueLabel: UILabel!
+    
+    @IBOutlet weak var methodLabel: UILabel!
+    @IBOutlet weak var typeSegment: UISegmentedControl!
+    
     
     var statusOptions: [String] = ["Available", "Unavailable", "Do Not Disturb"]
     
     var issueOptions: [String] = ["I'm feeling sad", "I'm having a rough day", "Physical Abuse", "Mental Abuse", "Substance Abuse", "I'm here to help someone else in need", "Other"]
     
+    var pickerOptions: [String] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         self.pickerInput.delegate = self
         self.pickerInput.dataSource = self
-     
+        updateView()
     }
-    
+    func updateView(){
+        let isAngel = UserSession.sharedInstance.isAngel()
+        
+        pickerOptions = isAngel ? statusOptions : issueOptions
+        severitySlider.isHidden = isAngel
+        severityLabel.isHidden = isAngel
+        severityInput.isHidden = isAngel
+        issueLabel.isHidden = isAngel
+        methodLabel.isHidden = isAngel
+        typeSegment.isHidden = isAngel
+    }
     @IBAction func comeBack(unwindSegue: UIStoryboardSegue){
         print("did it...came back")
     }
@@ -32,14 +49,14 @@ extension ConnectViewController: UIPickerViewDelegate, UIPickerViewDataSource{
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return self.issueOptions.count
+        return self.pickerOptions.count
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return self.issueOptions[row]
+        return self.pickerOptions[row]
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        print("picked: \(self.issueOptions[row])")
+        print("picked: \(self.pickerOptions[row])")
     }
 }
