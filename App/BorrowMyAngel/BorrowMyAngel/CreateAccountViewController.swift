7c13a9c0e955e541e2dc370d6c61727909146ad8
Ben Pomerenke <bpomerenke@gmail.com>
2018-11-04 02:00:13 -0600

    updating view to include shadow

---
+++ App/BorrowMyAngel/BorrowMyAngel/CreateAccountViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CreateAccountViewController.swift
+//  BorrowMyAngel
+//
+//  Created by dev1 on 11/4/18.
+//  Copyright © 2018 hack4good. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    @IBOutlet weak var shadowView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        shadowView.layer.cornerRadius = 10
+        // Do any additional setup after loading the view.
+    }
+
+}
