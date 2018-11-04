5d355728d432b1091a45f5e7682d9216abd6874c
Ben Pomerenke <bpomerenke@gmail.com>
2018-11-04 00:13:09 -0500

    removing angel workflow

--- App/BorrowMyAngel/BorrowMyAngel/ConnectViewController.swift
+++ App/BorrowMyAngel/BorrowMyAngel/ConnectViewController.swift
@@ -4,8 +4,16 @@ class ConnectViewController: UIViewController {
     
     @IBOutlet weak var severitySlider: UISlider!
     @IBOutlet weak var severityInput: UITextField!
+    @IBOutlet weak var pickerInput: UIPickerView!
+    
+    var statusOptions: [String] = ["Available", "Unavailable", "Do Not Disturb"]
+    
+    var issueOptions: [String] = ["I'm feeling sad", "I'm having a rough day", "Physical Abuse", "Mental Abuse", "Substance Abuse", "I'm here to help someone else in need", "Other"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.pickerInput.delegate = self
+        self.pickerInput.dataSource = self
      
     }
     
@@ -14,5 +22,24 @@ class ConnectViewController: UIViewController {
     }
     @IBAction func changeSeverity(_ sender: Any) {
         severityInput.text = String(Int(severitySlider.value*10.0))
+    }
+}
+
+extension ConnectViewController: UIPickerViewDelegate, UIPickerViewDataSource{
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.issueOptions.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return self.issueOptions[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print("picked: \(self.issueOptions[row])")
     }
 }
